bb8d2f16278000f3f776600b78fb4ec29d24e9cd
Nasim Ahmed <nashim.idb333@gmail.com>
2023-06-09 20:27:49 +0600

    update

--- resources/views/employeeSettings/designation/index.blade.php
+++ resources/views/employeeSettings/designation/index.blade.php
@@ -7,49 +7,49 @@
     <div class="row" id="table-bordered">
         <div class="col-12">
             <div class="card">
-                    <!-- table bordered -->
-                    <div class="table-responsive">
-                        <table class="table table-bordered mb-0">
-                            <a class="float-end" href="{{route(currentUser().'.designation.create')}}"style="font-size:1.7rem"><i class="bi bi-plus-square-fill"></i></a>
-                            <thead>
-                                <tr>
-                                    <th scope="col">{{__('#SL')}}</th>
-                                    <th scope="col">{{__('Designation')}}</th>
-                                    <th class="white-space-nowrap">{{__('ACTION')}}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @forelse($data as $p)
-                                <tr>
-                                <th scope="row">{{ ++$loop->index }}</th>
-                                    <td>{{$p->name}}</td>
-                                    <td class="white-space-nowrap">
-                                        <a href="{{route(currentUser().'.designation.edit',encryptor('encrypt',$p->id))}}">
-                                            <i class="bi bi-pencil-square"></i>
-                                        </a>
-                                        <a href="javascript:void()" onclick="$('#form{{$p->id}}').submit()">
-                                            <i class="bi bi-trash"></i>
-                                        </a>
-                                        <form id="form{{$p->id}}" action="{{route(currentUser().'.designation.destroy',encryptor('encrypt',$p->id))}}" method="post">
-                                            @csrf
-                                            @method('delete')
-                                            
-                                        </form>
-                                    </td>
-                                </tr>
-                                @empty
-                                <tr>
-                                    <th colspan="3" class="text-center">No Data Found</th>
-                                </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="my-3">
-                        {!! $data->links()!!}
-                    </div>
+                <!-- table bordered -->
+                <div class="table-responsive">
+                    <table class="table table-bordered mb-0">
+                        <a class="float-end" href="{{route(currentUser().'.designation.create')}}"style="font-size:1.7rem"><i class="bi bi-plus-square-fill"></i></a>
+                        <thead>
+                            <tr>
+                                <th scope="col">{{__('#SL')}}</th>
+                                <th scope="col">{{__('Designation')}}</th>
+                                <th class="white-space-nowrap">{{__('ACTION')}}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($data as $p)
+                            <tr>
+                            <th scope="row">{{ ++$loop->index }}</th>
+                                <td>{{$p->name}}</td>
+                                <td class="white-space-nowrap">
+                                    <a href="{{route(currentUser().'.designation.edit',encryptor('encrypt',$p->id))}}">
+                                        <i class="bi bi-pencil-square"></i>
+                                    </a>
+                                    <a class="text-danger" href="javascript:void()" onclick="$('#form{{$p->id}}').submit()">
+                                        <i class="bi bi-trash"></i>
+                                    </a>
+                                    <form id="form{{$p->id}}" onsubmit="return confirm('Are you sure?')" action="{{route(currentUser().'.designation.destroy',encryptor('encrypt',$p->id))}}" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        
+                                    </form>
+                                </td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <th colspan="3" class="text-center">No Data Found</th>
+                            </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="my-3">
+                    {!! $data->links()!!}
                 </div>
             </div>
+        </div>
     </div>
 </section>
 @endsection
