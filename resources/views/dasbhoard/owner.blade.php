5bbf615df6471c464f8ee9c35f7f570b7812a72e
BIPLAB UDDIN <biplabuddin990@gmail.com>
2024-02-01 18:42:56 +0600

    dashboard due,check done

--- resources/views/dasbhoard/owner.blade.php
+++ resources/views/dasbhoard/owner.blade.php
@@ -49,6 +49,17 @@
                 </div>
             </div>
        </div>
+       {{--  <div class="col-md-3 col-sm-6 col-lg-3">
+            <div class="info-box">
+                <span class="info-box-icon bg-red">
+                <i class="bi bi-dash-square icon"></i>
+                </span>
+                <div class="info-box-content">
+                    <span class="text-bold text-uppercase">Total Expense Amount</span>
+                    <span class="info-box-number">৳  0.00</span>
+                </div>
+            </div>
+       </div>  --}}
     </section>
     <section class="row">
        <div class="col-md-3 col-sm-6 col-lg-3">
@@ -102,7 +113,7 @@
                <div class="inner text-uppercase">
                     <h3>138</h3>
                     <p>Suppliers</p>
-               </div> 
+               </div>
                <div class="icon">
                 <i class="bi bi-people-fill"></i>
                </div>
@@ -116,7 +127,7 @@
                <div class="inner text-uppercase">
                     <h3>18</h3>
                     <p>Group</p>
-               </div> 
+               </div>
                <div class="icon">
                 <i class="bi bi-people-fill"></i>
                </div>
@@ -130,7 +141,7 @@
                <div class="inner text-uppercase">
                     <h3>18</h3>
                     <p>Invoice</p>
-               </div> 
+               </div>
                <div class="icon">
                 <i class="bi bi-receipt"></i>
                </div>
@@ -144,13 +155,83 @@
                <div class="inner text-uppercase">
                     <h3>198</h3>
                     <p>Memu</p>
-               </div> 
+               </div>
                <div class="icon">
                 <i class="bi bi-receipt"></i>
                </div>
                <a href="#" class="small-box-footer text-uppercase">View
                 <i class="bi bi-arrow-right-circle"></i>
                </a>
+            </div>
+       </div>
+       <div class="row">
+            <div class="col-6">
+                <div class="table-responsive">
+                    @php
+                    $todayCollection = \App\Models\Settings\ShopBalance::where('status', 0)->whereDate('new_due_date', now()->toDateString())->get();
+                    @endphp
+                    <h4 class="text-center bg-aqua text-white p-1">Today Due Collections</h3>
+                    <table class="table table-bordered mb-0">
+                        <thead>
+                            <tr>
+                                <th scope="col">{{__('#SL')}}</th>
+                                <th scope="col">{{__('Shop Name')}}</th>
+                                <th scope="col">{{__('Owner Name')}}</th>
+                                <th scope="col">{{__('Collection Taka')}}</th>
+                                <th scope="col">{{__('Sales Date')}}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($todayCollection as $data)
+                            <tr>
+                                <th scope="row">{{ ++$loop->index }}</th>
+                                <td>{{$data->shop?->shop_name}}</td>
+                                <td>{{$data->shop?->owner_name}}</td>
+                                <td>{{$data->balance_amount}}</td>
+                                <td>{{$data->created_at->format('d F Y')}}</td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <th colspan="5" class="text-center">No Data Found</th>
+                            </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="table-responsive">
+                    @php
+                    $todayCheck = \App\Models\Sales\SalesPayment::where('cash_type', 0)->whereDate('check_date', now()->toDateString())->get();
+                    @endphp
+                    <h4 class="text-center bg-aqua text-white p-1">Today Check Collections</h3>
+                    <table class="table table-bordered mb-0">
+                        <thead>
+                            <tr>
+                                <th scope="col">{{__('#SL')}}</th>
+                                <th scope="col">{{__('Shop Name')}}</th>
+                                <th scope="col">{{__('Owner Name')}}</th>
+                                <th scope="col">{{__('Collection Taka')}}</th>
+                                <th scope="col">{{__('Check Receive Date')}}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse($todayCheck as $c)
+                            <tr>
+                                <th scope="row">{{ ++$loop->index }}</th>
+                                <td>{{$c->shop?->shop_name}}</td>
+                                <td>{{$c->shop?->owner_name}}</td>
+                                <td>{{$c->amount}}</td>
+                                <td>{{$c->created_at->format('d F Y')}}</td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <th colspan="5" class="text-center">No Data Found</th>
+                            </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
             </div>
        </div>
     </section>
